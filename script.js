c3c7e1af6e5e185e02b1f06678c547de966ec2a3
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-05-19 22:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+setInterval(()=>{
+	rot();
+},100);
+function rot() {
+line.style.transform = 'rotate(2deg)';
+}
+	
+	
